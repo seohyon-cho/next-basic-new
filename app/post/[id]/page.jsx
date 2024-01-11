1ea843ee0c76456ad9cef4ca863a3ddd9196fa8d
seohyon-cho <flanique@naver.com>
2024-01-11 15:19:00 +0900

    상세 페이지에 데이터 출력

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,21 +1,27 @@
 'use client';
+import styles from './detail.module.scss';
 import { useState, useEffect } from 'react';
+import clsx from 'clsx';
 
 export default function PostDetail({ params }) {
-  const { id } = params;
-  const [PostEl, setPostEl] = useState(null);
+	const { id } = params;
+	const [PostEl, setPostEl] = useState(null);
 
-  useEffect(() => {
-    fetch(`/api/requestPost/${id}`)
-      .then((data) => data.json())
-      .then((json) => {
-        console.log(json);
-      });
-  }, [id]);
+	useEffect(() => {
+		fetch(`/api/requestPost/${id}`)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+				setPostEl(json.result);
+			});
+	}, [id]);
 
-  return (
-    <div>
-      <h2>{id}번째 게시글 상세페이지</h2>
-    </div>
-  );
+	return (
+		<section className={clsx(styles.detail)}>
+			<article>
+				<h2>{PostEl?.title}</h2>
+				<p>{PostEl?.content}</p>
+			</article>
+		</section>
+	);
 }
